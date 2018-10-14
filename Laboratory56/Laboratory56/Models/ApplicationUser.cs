7474ca338b127f4da49f18573ad2c458196964c8
EugeneTM <j.atisto@gmail.com>
2018-10-14 22:55:00 +0600

    added the functionto add an avatar for view Registra

---
+++ Laboratory56/Laboratory56/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Laboratory56.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string AvatarImage { get; set; } // Путь картинки
+    }
+}
