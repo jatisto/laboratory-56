5ad0c94514e97e1075d3cb0406f85efeb59f0140
EugeneTM <j.atisto@gmail.com>
2018-10-23 20:18:21 +0600

    modify

--- Laboratory56/Laboratory56/Models/Publication.cs
+++ Laboratory56/Laboratory56/Models/Publication.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace Laboratory56.Models
     public class Publication
     {
         public int Id { get; set; }
+        [Display(Name = "Изображение")]
         public string ImageUrl { get; set; }
+        [Display(Name = "Описание")]
         public string Description { get; set; }
 
         public int Like { get; set; }
